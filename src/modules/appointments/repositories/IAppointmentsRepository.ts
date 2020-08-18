4351c6f08d64ed3ed936dc16d145deb2c2db6f0b
Falkaniere <jonatasfalkaniere@gmail.com>
2020-08-17 23:56:51 -0300

    refactoring

---
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -0,0 +1,7 @@
+import Appointment from '../infra/typeorm/entities/Appointment'
+import ICreateAppoinmentDTO from '../dtos/ICreateAppointmentDTO';
+
+export default interface IAppointmentsRepository {
+  create(data: ICreateAppoinmentDTO): Promise<Appointment>;
+  findByDate(date: Date): Promise<Appointment | undefined>
+}
